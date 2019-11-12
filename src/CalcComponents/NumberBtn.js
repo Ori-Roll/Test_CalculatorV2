17454eba3f51b66bf920a7a9a93ce1647ac4ed8f
Ori Roll <oriroll@gmail.com>
2019-11-12 21:18:48 +0200

    Added a CalcNumPad component

--- src/CalcComponents/NumberBtn.js
+++ src/CalcComponents/NumberBtn.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+function btnClick(btnValue){
+    console.log("THIS IS MEEEEE!!!:"+btnValue);
+};
+
 function NumberBtn(props){
 
     return(
-        <button type="button" className="numberBtn" style={{order: props.btnPlace}}>{props.btnValue}</button>
+        <button type="button" className="numberBtn" style={{order: props.btnPlace}} onClick={btnClick(props.btnValue)}>{props.btnValue}</button>
     );
 
 };
