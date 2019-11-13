6e551f66e237f257ef23da8bac99159dc5d0fc50
Ori Roll <oriroll@gmail.com>
2019-11-13 08:59:38 +0200

    btnClick now runs only when onClicked

--- src/CalcComponents/NumberBtn.js
+++ src/CalcComponents/NumberBtn.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-function btnClick(btnValue){
-    console.log("THIS IS MEEEEE!!!:"+btnValue);
+function btnClick(e, btnValue){
+    console.log("It's-a-meee, "+btnValue+".", e);
 };
 
 function NumberBtn(props){
 
     return(
-        <button type="button" className="numberBtn" style={{order: props.btnPlace}} onClick={btnClick(props.btnValue)}>{props.btnValue}</button>
+        <button type="button" className="numberBtn" style={{order: props.btnPlace}} onClick={(e) => {btnClick(e, props.btnValue)}}>{props.btnValue}</button>
     );
 
 };
