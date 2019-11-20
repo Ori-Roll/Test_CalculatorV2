f667ad67a8d2e44d804037487386cdbbf870f477
Ori Roll <oriroll@gmail.com>
2019-11-20 15:44:59 +0200

    Moved key clicked (changeNumOnScreen) to parent (from NumberBtn to CalcBody)

--- src/CalcComponents/CalcBody.js
+++ src/CalcComponents/CalcBody.js
@@ -7,8 +7,64 @@ function CalcBody(){
     
     const [numOnScreen, setNumOnScreen] = useState("0.0");
 
-    function changeNumOnScreen(changeReason){
-        setNumOnScreen(changeReason);
+    function changeNumOnScreen(btnValue){
+        let onScreenNum = numOnScreen;
+
+        switch(btnValue) {
+            /* case Number.isNaN(btnValue):     <<<<< Dosnt work because of Number.isNaN - +/-/X/=  comes out as NUMBERS!!! 
+                console.log("is NaN");      
+                switch(btnValue) {
+                    case "+":
+                        console.log("+ function")
+                    break;
+                    case "-":
+                        console.log("- function")
+                    break;
+                    case "X":
+                        console.log("x function")
+                    break;
+                    case "÷": 
+                        console.log("÷ function")
+                    break;                
+                    case "=": 
+                        console.log("= function")
+                    break;
+                    case "÷": 
+                        console.log(". function")
+                    break;
+                    default:
+                        console.log("OOPS, Somthing else")
+                }
+            break; */
+            case "+":
+                console.log("+ - add function");
+            break;
+            case "-":
+                console.log("- - subtract function");
+            break;
+            case "x":
+                console.log("x - multiply function");
+            break;
+            case "÷":
+                console.log("÷ - divide function");
+            break;
+            case "=":
+                console.log("= - equals function");
+            break;
+            case ".":
+                console.log(". - point function");
+            break;
+    
+            default:
+                if ((onScreenNum*10) > 9999 || (onScreenNum*10) < -9999){
+                    console.log("TOO BIG Or SMALL!");
+                } else {
+                    onScreenNum =((onScreenNum*10)+Number(btnValue));
+                    console.log("DISPLAY: "+onScreenNum);
+                    setNumOnScreen(onScreenNum);
+                }
+            break;
+        }
     };
     
     return(
--- src/CalcComponents/NumberBtn.js
+++ src/CalcComponents/NumberBtn.js
@@ -3,69 +3,11 @@ import { isNumber } from "util";
 
 let onScreenNum = 0.0;
 
-
-
 function NumberBtn(props){
 
     function btnClick(e, btnValue){
         console.log("It's-a-meee, "+btnValue+"."/* ,e */ , "target: "+e.target);
-                
-        switch(btnValue) {
-            /* case Number.isNaN(btnValue):     <<<<< Dosnt work because of Number.isNaN - +/-/X/=  comes out as NUMBERS!!! 
-                console.log("is NaN");      
-                switch(btnValue) {
-                    case "+":
-                        console.log("+ function")
-                    break;
-                    case "-":
-                        console.log("- function")
-                    break;
-                    case "X":
-                        console.log("x function")
-                    break;
-                    case "÷": 
-                        console.log("÷ function")
-                    break;                
-                    case "=": 
-                        console.log("= function")
-                    break;
-                    case "÷": 
-                        console.log(". function")
-                    break;
-                    default:
-                        console.log("OOPS, Somthing else")
-                }
-            break; */
-            case "+":
-                console.log("+ - add function");
-            break;
-            case "-":
-                console.log("- - subtract function");
-            break;
-            case "x":
-                console.log("x - multiply function");
-            break;
-            case "÷":
-                console.log("÷ - divide function");
-            break;
-            case "=":
-                console.log("= - equals function");
-            break;
-            case ".":
-                console.log(". - point function");
-            break;
-    
-            default:
-                if ((onScreenNum*10) > 9999 || (onScreenNum*10) < -9999){
-                    console.log("TOO BIG Or SMALL!");
-                } else {
-                    onScreenNum =((onScreenNum*10)+Number(btnValue));
-                    console.log("DISPLAY: "+onScreenNum);
-                    console.log(props.changeNumOnScreen)
-                    props.changeNumOnScreen(onScreenNum);
-                }
-            break;
-        }
+        props.changeNumOnScreen(btnValue);
     };
 
     return(
