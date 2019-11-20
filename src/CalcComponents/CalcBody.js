ffda5b166b9173d8bfc8f2d33f0fa7c5dd771af1
Ori Roll <oriroll@gmail.com>
2019-11-20 12:36:18 +0200

    New state for the screen display (empty for now)

--- src/CalcComponents/CalcBody.js
+++ src/CalcComponents/CalcBody.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import NumberBtn from "./NumberBtn.js";
 import CalcNumPad from "./CalcNumPad.js"
 import CalcScreen from "./CalcScreen.js"
 
 function CalcBody(){
+    
+    const [numOnScreen, setNumOnScreen] = useState("0.0");
+
+    
     return(
         <div className="calcBody">
             <CalcScreen/>
