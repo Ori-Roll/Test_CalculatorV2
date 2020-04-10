495ccc1dbac1dfa0ec2f1e66d8067ab366fd0211
Ori Roll <oriroll@gmail.com>
2020-04-10 14:26:39 +0300

    Updates

---
+++ precache-manifest.6e3f1dab50640d14c1911a3a1f6357cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391aff2adce341f01291adebb2368780",
+    "url": "/Test_CalculatorV2/index.html"
+  },
+  {
+    "revision": "f9f7bbfc43b4438c6195",
+    "url": "/Test_CalculatorV2/static/css/main.d5d06c4e.chunk.css"
+  },
+  {
+    "revision": "6ba18a38bc1931b828a5",
+    "url": "/Test_CalculatorV2/static/js/2.ac6b0663.chunk.js"
+  },
+  {
+    "revision": "f9f7bbfc43b4438c6195",
+    "url": "/Test_CalculatorV2/static/js/main.cabe6c77.chunk.js"
+  },
+  {
+    "revision": "67847c1292657536f082",
+    "url": "/Test_CalculatorV2/static/js/runtime-main.daf7878d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Test_CalculatorV2/static/media/logo.25bf045c.svg"
+  }
+]);
